2ecf4ca4623f83497c22546e4c08fd2e87b54461
Chase van Hekken <cvh1717@gmail.com>
2013-10-26 12:59:02 -0400

    Ch. 5 Full Name Greeting

---
+++ fullnamegreeting.rb
@@ -0,0 +1,11 @@
+puts 'Hello there, and what\'s your first name?'
+firstName = gets.chomp
+
+puts 'What\'s your middle name?'
+middleName = gets.chomp
+
+puts 'What\'s your last name?'
+lastName = gets.chomp
+
+puts 'Your name is ' + firstName + ' ' + middleName + ' ' + lastName + '? What a lovely name!'
+puts 'Pleased to meet you, ' + firstName + ' ' + middleName + ' ' + lastName + '. :)'
